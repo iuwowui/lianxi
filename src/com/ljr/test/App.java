d29ffa8cf8ea8253dff0353142ae918db64cb58e
ljr <709113980@qq.com>
2019-05-28 13:54:46 +0800

    infomation

--- src/com/ljr/test/App.java
+++ src/com/ljr/test/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("456");
+        System.out.println("111");
+        System.out.println("111");
     }
 }
